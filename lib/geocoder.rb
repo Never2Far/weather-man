a14ab5070afb7e28cf6bdb9a21ae49f88ccf7dc5
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-07-29 15:58:35 -0500

    added more location info retrieved from API

--- lib/geocoder.rb
+++ lib/geocoder.rb
@@ -5,40 +5,70 @@
 
 
 require 'httparty'
-# require 'pry'
+require 'pry'
 require 'open-uri'
 require 'json'
-require 'nokogiri'
+# require 'nokogiri'
 
-def Geocoder
+class Geocoder
 
-    $base_url = "http://www.mapquestapi.com/geocoding/v1/address?key=jD1z4gMfiupsX29tyYAdeEs5jW4jB3Id&location="
+    # $base_url = "http://www.mapquestapi.com/geocoding/v1/address?key=jD1z4gMfiupsX29tyYAdeEs5jW4jB3Id&location="
 
     attr_accessor :input, :output
+    attr_reader :base_url
 
 
     def initialize
-        @input = nil
-        @output = nil
+        @base_url = "http://www.mapquestapi.com/geocoding/v1/address?key=jD1z4gMfiupsX29tyYAdeEs5jW4jB3Id&location="
+        @input
+        @output
         # unless input_string == nil
         #     @output = geocode(input_string)
         # end
     end
 
 
-    def geocode(input_string, decimals = 4)
-        url = $base_url + input_string
+    def geocode(search_string, decimals = 4)
+        @input = search_string
+        url = @base_url + @input
         response = HTTParty.get(url)
-        @output = response["results"][0]["locations"][0]["latLng"]
-        @output["lat"] = @output["lat"].round(4)
-        @output["lng"] = @output["lng"].round(4)
-        return @output
+        binding.pry
+        location_hash = response["results"][0]["locations"][0]
+        @output = {
+                    :street => [location_hash]["street"],
+                    :neighborhood => [location_hash]["adminArea6"],
+                    :city => [location_hash]["adminArea5"],
+                    :county => [location_hash]["adminArea4"],
+                    :state => [location_hash]["adminArea3"],
+                    :zip => [location_hash]["postalCode"],
+                    :country => [location_hash]["adminArea1"],
+                    :lat => [location_hash]["latLng"]["lat"].round(4),
+                    :lng => [location_hash]["latLng"]["lng"].round(4),
+                    :coordinates => [location_hash]["lng"],
+                    :unknownInput => [location_hash]["unknownInput"],
+                    :mapUrl => [location_hash]["mapUrl"]
+                 }
+        # response["results"][0]["locations"][0]["latLng"]
+        lat = @output["lat"].round(4)
+        lng = @output["lng"].round(4)
+        return [lat, lng]
+    end
+
+    def reverse(lat, lng)
+        
+    end
+
+    def location_info
+
     end
 
 
 
 
+
 end
+
+
 
 geocoder = Geocoder.new
 
