56b6e73cdd44f2302d05f30317cd2d98ee4fa584
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-07-30 11:06:48 -0500

    A user has many locations/can view a list of them

--- lib/user.rb
+++ lib/user.rb
@@ -4,10 +4,11 @@ class User
 
 
 
-    attr_accessor :name
+    attr_accessor :name, :locations
 
     def initialize(name)
         @name = name
+        @locations = []
         save
     end
 
@@ -18,6 +19,18 @@ class User
     def self.all
         return @@all
     end
+
+    def add(location)
+        @locations << location
+    end
+
+    def list_locations
+        puts "Here is a list of your saved locations:"
+        puts "---------------------------------------"
+        locations.each_with_index {|location, index|
+        puts "#{index + 1}. #{location.name}"}
+    end
+
 
 
 end
