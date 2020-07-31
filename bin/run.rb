4b9d44a54e27178f12f76e684e3a1a914065f0d3
Never2Far <30479783+Never2Far@users.noreply.github.com>
2020-07-31 09:16:44 -0500

    Creates a user,  geocodes and saves given location

--- bin/run.rb
+++ bin/run.rb
@@ -6,61 +6,60 @@ require 'pry'
 
 
 # geocoder = Geocoder.new
+# attributes = geocoder.geocode("1016 Laval Dr.")
+# name = "Home"
+# home = Location.new(name, attributes)
+# binding.pry
 
-# puts "Please enter a location:"
-# input = gets.strip
+new_user = User.intro
 
-# output = geocoder.geocode(input)
-# puts output
+# module Interface
 
+    # def intro
+    #     puts "What is your name?"
+    #     username = gets.strip
+    #     user = User.new(username)
+    #     puts ""
+    #     puts ""
+    #     puts "Welcome, #{username}!"
+    # end
+input = nil
+    while input != "4"
+        puts "Please select from the following (enter 1-4):"
+        puts "--------------------------------------------"
+        puts "1. Add new location"
+        puts "2. View saved locations"
+        puts "3. Get Forecast"
+        puts "4. Exit"
+        input = gets.strip
 
-
-geocoder = Geocoder.new
-attributes = geocoder.geocode("1016 Laval Dr.")
-name = "Home"
-home = Location.new(name, attributes)
-binding.pry
-
-def intro
-    puts "What is your name?"
-    username = gets.strip
-    user = User.new(username)
-    puts ""
-    puts ""
-    puts "Welcome, #{username}!"
-end
-
-def main_menu
-    puts "Please select from the following (enter 1-3):"
-    puts "1. Add new location"
-    puts "2. View saved locations"
-    puts "3. Get Forecast"
-    input = gets.strip
-
-    case input
-    when 1
-        puts "Enter a name for this location (ex. Home, Work, etc.):"
-        name = gets.strip
-        puts "Where is #{name}?"
-        loc = gets.strip
-        geocoder = Geocoder.new
-        attributes = geocoder.geocode(loc)
-        new_loc = Location.new(name, attributes)
-        new_loc.display_info
-        puts ""
-        puts "Would you like to save #{name}? (y/n)"
-        answer = gets.strip
-        if (answer.downcase == "y") || (answer.downcase == "yes")
-            user.save_location(new_loc)
-            puts "#{name} has been added to your list of locations!"
-            main_menu
+        case input
+        when "1"
+            puts "Enter a name for this location (ex. Home, Work, etc.):"
+            name = gets.strip
+            puts "Where is #{name}?"
+            loc = gets.strip
+            geocoder = Geocoder.new
+            attributes = geocoder.geocode(loc)
+            new_loc = Location.new(name, attributes)
+            new_loc.display_info
+            puts ""
+            puts "Would you like to save #{name}? (y/n)"
+            answer = gets.strip
+            if (answer.downcase == "y") || (answer.downcase == "yes")
+                new_user.add_location(new_loc)
+                puts "#{name} has been added to your list of locations!"
+                # main_menu
+            else
+                # main_menu
+            end
+        when "2"
+            new_user.list_locations
+            # main_menu
+        when "4"
+            exit
         else
-            main_menu
+            # main_menu
         end
-    when 2
-        user.list_locations
-        main_menu
-    else
-        main_menu
     end
-end
+# end
